b7627a62118cf431ca625ab9c0bdb5740cb6e4bc
fatimah azzahra <faazaa26@gmail.com>
2023-03-23 21:17:26 +0700

    pertemuan4

---
+++ pertemuan4-/admin/Daftar.php
@@ -0,0 +1,18 @@
+
+<?php
+include_once 'templates/header.php';
+include_once 'templates/topbar.php';
+include_once 'templates/sidebar.php';
+
+?>
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Silahkan Isi form Pendaftaran</h1>
+                        <hr>
+                    </div>
+                </main>
+
+<?php
+include_once 'templates/footer.php';
+?>
+                
